8b18b47cc4948c9492d7943550645c430e36f6d9
Mohamed Ali <mohammed44frx@gmail.com>
2026-01-30 15:02:57 +0200

    checkpoint/ start of creating the sidebar

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -4,9 +4,6 @@ import Home from "./pages/Home.jsx";
 function App() {
   return (
     <>
-      <div className="text-yellow-400">
-        This is The frontend/client part of the application
-      </div>
       <Home />
     </>
   );
---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,7 @@
+export default function Sidebar() {
+  return (
+    <nav className="bg-blue-500 p-4">
+      <h1 className="text-white text-2xl">Sidebar</h1>
+    </nav>
+  );
+}
